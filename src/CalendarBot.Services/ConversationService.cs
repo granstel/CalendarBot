33198e052816daa7ed150c51023c7d4c0e650936
Stepan Grankin <sgrankin@inbox.ru>
2020-03-26 11:03:31 +0300

    Start to process parameters from DF

--- src/CalendarBot.Services/ConversationService.cs
+++ src/CalendarBot.Services/ConversationService.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using CalendarBot.Models.Internal;
 using GranSteL.Helpers.Redis;
@@ -21,6 +24,27 @@ namespace CalendarBot.Services
         public async Task<Response> GetResponseAsync(Request request)
         {
             var dialog = await _dialogflowService.GetResponseAsync(request);
+
+            var datePeriodsString = dialog?.Parameters?.Where(p => string.Equals(p.Key, "date-period")).Select(p => p.Value).FirstOrDefault();
+
+            if(!string.IsNullOrEmpty(datePeriodsString))
+            {
+                var periodsStrings = datePeriodsString.Split('/');
+
+                var dates = periodsStrings.Select(s =>
+                {
+                    if (DateTime.TryParse(s, out var date))
+                        return date;
+                    return default(DateTime?);
+                }).Where(d => d.HasValue).OrderBy(d => d).ToList();
+
+                var year = dates.Select(d => d?.Year).FirstOrDefault();
+
+                if(year.HasValue)
+                {
+                    _cache.TryGet($"Calendar:{year}", out Dictionary<string, Month> calendar);
+                }
+            }
 
             var response = new Response { Text = dialog.Response };
 
