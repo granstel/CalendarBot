7c247a5813841d802cee4943d5f1eddbaace2e95
Stepan Grankin <sgrankin@inbox.ru>
2020-04-26 23:07:53 +0300

    Add metrics

--- src/CalendarBot.Api/Middleware/ExceptionsMiddleware.cs
+++ src/CalendarBot.Api/Middleware/ExceptionsMiddleware.cs
@@ -28,6 +28,5 @@ namespace CalendarBot.Api.Middleware
                 throw;
             }
         }
-
     }
 }
--- src/CalendarBot.Api/Program.cs
+++ src/CalendarBot.Api/Program.cs
@@ -1,14 +1,33 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using NLog;
 using NLog.Web;
+using System;
 
 namespace CalendarBot.Api
 {
     public static class Program
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            try
+            {
+                BuildWebHost(args).Run();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+
+                throw;
+            }
+            finally
+            {
+                Zidium.Api.Client.Instance.EventManager.Flush();
+                Zidium.Api.Client.Instance.WebLogManager.Flush();
+
+                LogManager.Shutdown();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
