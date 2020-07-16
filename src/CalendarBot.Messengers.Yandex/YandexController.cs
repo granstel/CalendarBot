d3dd70bf4a47b47260aba4ebf49fedafdff0f0c0
Stepan Grankin <sgrankin@inbox.ru>
2020-07-16 14:53:03 +0300

    use route attribute on common controller

--- src/CalendarBot.Messengers.Yandex/YandexController.cs
+++ src/CalendarBot.Messengers.Yandex/YandexController.cs
@@ -5,7 +5,6 @@ using Yandex.Dialogs.Models.Input;
 namespace CalendarBot.Messengers.Yandex
 {
     [Produces("application/json")]
-    [Route("/Yandex")]
     public class YandexController : MessengerController<InputModel, OutputModel>
     {
         public YandexController(IYandexService yandexService, YandexConfiguration configuration) : base(yandexService, configuration)
